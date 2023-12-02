d2f5b996d006d8e848f54cbf8012bb4d29bada9e
AidanV <aidanvanduyne@gmail.com>
2023-12-02 21:45:13 +0000

    finished part 2

--- bin/main.ml
+++ bin/main.ml
@@ -1,24 +1,13 @@
-(*
-
-split on ; 
-
-regexp match [0-9] $color
-
-nums are less than specified
-
-fold &&
-
-accumulate the valid lines
-
-*)
-
-
 let file = "input.dat"
 
 let read_lines file =
   let contents = In_channel.with_open_bin file In_channel.input_all in
   String.split_on_char '\n' contents
 
+let rec find_max_tuple tuples (ma, mb, mc) =
+  match tuples with
+  | [] -> (ma, mb, mc)
+  | (a, b, c)::tuples -> find_max_tuple tuples (max a ma, max b mb, max c mc) 
 
 let num_color s color = 
   let rgx = Str.regexp ({|\([0-9]+\) |}^color) in
@@ -27,21 +16,20 @@ let num_color s color =
       Str.matched_group 1 s |> int_of_string
   with _ -> 0
 
+let product (a, b, c) = a * b * c 
 
-let is_turn_valid s = 
-  num_color s "red" <= 12 && num_color s "green" <= 13 && num_color s "blue" <= 14
+let get_turn_tuple s = 
+  (num_color s "red", num_color s "green", num_color s "blue")
 
-let is_line_valid s = 
-  String.split_on_char ';' s |>
-  List.map is_turn_valid |>
-  List.fold_left (&&) true
+let line_max s = 
+  (String.split_on_char ';' s |>
+  List.map get_turn_tuple |>
+  find_max_tuple) (0, 0, 0)
 
-let rec acc_valid_lines num lines = 
+let rec acc_valid_lines lines = 
   match lines with
   | [] -> 0
   | line::lines -> 
-    if is_line_valid line
-    then num + (acc_valid_lines (num + 1) lines) 
-    else acc_valid_lines (num + 1) lines
+    product (line_max line) +  acc_valid_lines lines
 
-let () = read_lines file |> List.filter ((<>) "") |> acc_valid_lines 1 |> string_of_int |> print_endline
+let () = read_lines file |> List.filter ((<>) "") |> acc_valid_lines |> string_of_int |> print_endline
