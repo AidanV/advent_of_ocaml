fb7f8967ca7ca19313148194d0b9145d19c16734
AidanV <aidanvanduyne@gmail.com>
2023-12-04 07:43:29 +0000

    finished part 2

--- bin/main.ml
+++ bin/main.ml
@@ -1,65 +1,19 @@
-(*
-find all symbol positions -> hashmap?
-
-find nums by splitting on all char not num
-
-nums (list positions) value
-*)
-
-(* let find_numbers_with_symbols numbers symbols =*)
-
-
-(* let convert_non_digit_to_space c = 
-    match c with
-    | '0'..'9' -> c
-    | _ -> ' '*)
-
-let is_symbol c =
+let is_gear c = 
   match c with
-  | '0'..'9' | '.' -> false
-  | _ -> true
+  | '*' -> true
+  | _ -> false
 
 let is_digit c = 
   match c with
   | '0'..'9' -> true
   | _ -> false
 
-let is_symbol_around line_num symbols char_num = 
-  List.fold_left (fun prev (l_num, c_num) -> (abs (line_num - l_num) <= 1 &&  abs (char_num - c_num) <= 1) || prev ) false symbols
 
-
-let rec get_number is_around char_num chars = 
-  match chars with
-  | [] -> ("", false, [], char_num + 1)
-  | x::chars when not (is_digit x) -> ("", false, chars, char_num + 1)
-  | c::chars ->
-    let (rest_num, around, ret_chars, curr_pos)  = get_number is_around (char_num+1) chars in
-    (String.make 1 c ^ rest_num, is_around (char_num) || around, ret_chars, curr_pos)
-(*returns option and rest of array*)
-  
-let rec get_all_numbers_from_line line_num line_chars curr_char_pos symbols =
-  let is_around = is_symbol_around line_num symbols in
-  let num = get_number (is_around) curr_char_pos line_chars in
-  match num with
-  | (num_string, around, [], _) ->
-    if around 
-    then [int_of_string num_string] 
-    else []
-  | (num_string, around, rest_chars, char_pos) ->
-    if around 
-    then (int_of_string num_string)::(get_all_numbers_from_line line_num rest_chars char_pos symbols) 
-    else get_all_numbers_from_line line_num rest_chars char_pos symbols
-(*
-build up a number and only keep it if one of the values is next to a symbol
-*)
-  
-  
-
-let get_all_symbols_from_line line_num line = 
+let get_all_gears_from_line line_num line = 
   String.to_seq line|>
   List.of_seq |>
   List.mapi((fun i c -> (c, i))) |>
-  List.filter (fun (c, _) -> is_symbol c) |>
+  List.filter (fun (c, _) -> is_gear c) |>
   List.map (fun (_, i) -> (line_num, i))
   
 
@@ -68,14 +22,85 @@ let read_lines file =
   String.split_on_char '\n' contents
 
 
+(*includes original ch*)
+let rec find_num_left line (ch:int) = 
+  try
+    match line.[ch] with
+    | '0'..'9' -> find_num_left line (ch - 1) ^ String.make 1 line.[ch]
+    | _ -> ""
+  with _ -> ""
+
+let rec find_num_right line (ch:int) = 
+  try
+    match line.[ch] with
+    | '0'..'9' -> String.make 1 line.[ch] ^ find_num_right line (ch + 1)
+    | _ -> ""
+  with _ -> ""
+
+let find_number line ch =
+  
+  try 
+    let c = (line).[ch] in
+    let left = find_num_left line ch in 
+    let start = ch - String.length left + 1 in
+    let right = find_num_right line (ch + 1) in
+    if is_digit c then Some (start, left ^ right) else None
+  with _ -> None
+
+let is_not_already_in_list (a, b, c) list = 
+  List.fold_left (fun prev (d, e, f) -> not (a = d && b = e && c = f) && prev) true list
+
+let make_gear_set (lines: string list) gear = 
+  let (gear_line, gear_ch) = gear in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line - 1)) (gear_ch - 1) with
+    | Some(start, num_string) -> [(gear_line - 1, start, num_string)] 
+    | None -> []
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line - 1)) (gear_ch) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line - 1, start, num_string) gear_numbers then (gear_line - 1, start, num_string)::gear_numbers else gear_numbers
+    | None -> gear_numbers
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line - 1)) (gear_ch + 1) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line - 1, start, num_string) gear_numbers then (gear_line - 1, start, num_string)::gear_numbers else gear_numbers
+    | None -> gear_numbers
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line)) (gear_ch - 1) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line, start, num_string) gear_numbers then (gear_line, start, num_string)::gear_numbers else  gear_numbers
+    | None -> gear_numbers
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line)) (gear_ch + 1) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line, start, num_string) gear_numbers then (gear_line, start, num_string)::gear_numbers else gear_numbers
+    | None -> gear_numbers
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line + 1)) (gear_ch - 1) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line + 1, start, num_string) gear_numbers then (gear_line + 1, start, num_string)::gear_numbers else gear_numbers
+    | None -> gear_numbers
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line + 1)) (gear_ch) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line + 1, start, num_string) gear_numbers then (gear_line + 1, start, num_string)::gear_numbers else gear_numbers
+    | None -> gear_numbers
+  in
+  let gear_numbers = 
+    match find_number (List.nth lines (gear_line + 1)) (gear_ch + 1) with
+    | Some(start, num_string) -> if is_not_already_in_list (gear_line + 1, start, num_string) gear_numbers then (gear_line + 1, start, num_string)::gear_numbers else gear_numbers
+    | None -> gear_numbers
+  in
+  List.map (fun (_, _, num) -> int_of_string num) gear_numbers 
+  
+
 let () = 
   let lines = read_lines "input.dat" |> List.filter ((<>) "") in 
-  let symbols =  lines |> List.mapi (fun i s -> get_all_symbols_from_line i s) |> List.flatten in
-  lines |> List.mapi (fun i line -> get_all_numbers_from_line i (List.of_seq (String.to_seq line)) 0 symbols) |> List.flatten 
-  |> List.fold_left (+) 0
-  |> string_of_int
-  |> print_endline
-  
-  (* get_all_numbers_from_line
-  (**) find_numbers_with_symbols 
-  (lines |> List.mapi (fun i s -> get_all_numbers_from_line i (List.of_seq (String.to_seq s)) |> List.flatten) *)
+  let gears =  lines |> List.mapi (fun i s -> get_all_gears_from_line i s) |> List.flatten in
+  List.map (fun gear -> make_gear_set lines gear) gears |>
+  List.filter (fun ls -> List.length ls > 1) |>
+  List.map (fun ls -> List.fold_left ( * ) 1 ls) |>
+  List.fold_left (+) 0 |>
+  string_of_int |>
+  print_endline
