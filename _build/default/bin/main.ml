7ee69a196b48997cc168992702890e7804f533f6
AidanV <aidanvanduyne@gmail.com>
2023-12-07 00:07:06 +0000

    finished part 2

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -38,7 +38,7 @@ let rec split_list_on_val list (v: string) =
 ;;
 
 
-let find_map_in_range (v: int) (map_ranges: map_range list) =
+(*let find_map_in_range (v: int) (map_ranges: map_range list) =
   let valid_ranges = List.filter (
     fun {map = _; range = (l, h)} ->
       (l <= v && v <= h)
@@ -46,34 +46,65 @@ let find_map_in_range (v: int) (map_ranges: map_range list) =
   match valid_ranges with
   | x::_ -> x.map
   | [] -> (+) 0
+;;*)
+
+type bound = Lower of int * (int -> int) | Upper of int * (int -> int) | None;;
+
+let get_next_map_range (map_ranges: map_range list) (v: int) =
+  match List.find_opt (fun {map=_ ; range = (_, ub)} -> ub >= v) map_ranges with
+  | Some {map = m; range = lb, ub} -> if lb > v then Lower (lb, m) else Upper (ub, m)
+  | None -> None
+;;
+
+let rec get_mapped_ranges (map_ranges: map_range list) (input_ranges: (int * int) list) =
+  match input_ranges with
+  | [] -> []
+  | (lb, ub)::rest -> 
+    match get_next_map_range map_ranges lb with
+    | Lower (nxt, _) ->
+      if nxt > ub then (* was >*)
+        (lb, ub)::(get_mapped_ranges map_ranges rest)
+      else
+        (lb, nxt-1)::(get_mapped_ranges map_ranges ((nxt, ub)::rest))
+    | Upper (nxt, m) ->
+      if nxt >= ub then (* was >*)
+        (m lb, m ub)::(get_mapped_ranges map_ranges rest)
+      else
+        (m lb, m (nxt))::(get_mapped_ranges map_ranges ((nxt+1, ub)::rest))
+    | None ->
+      (lb, ub)::rest
+;;
+
+let get_paired_list ls =
+  let left = List.filteri(fun i _ -> i mod 2 = 0) ls in
+  let right = List.filteri(fun i _ -> (i + 1) mod 2 = 0) ls in
+  List.combine left right
+;;
+
+let convert_to_upper_bound (start, range: int*int) =
+  (start, start+range - 1)
 ;;
 
 let () = 
   let lines = read_lines "input.dat" in
-  let seeds = List.hd lines |> String.split_on_char ' ' |> List.tl |> List.map int_of_string in
+  let seeds = List.hd lines |> String.split_on_char ' ' |> List.tl |> List.map int_of_string |> get_paired_list in
+  let lb_ub_seeds = List.map (convert_to_upper_bound) seeds |> List.sort(fun (l, _) (r, _) -> l - r) in
   let lines = List.tl lines |> List.tl in
   let splited = split_list_on_val lines "" in
   let splited = List.filter(fun a -> List.length a > 0) splited in
   let splited = List.map (List.tl) splited in
-  let map_ranges = List.map (List.map make_map_range) splited in
-  let locations = List.map (
-    fun seed ->
-    List.fold_left (
-      print_endline "";
-      fun prev map_range -> 
-      let map = find_map_in_range prev map_range in
+  let all_map_ranges = List.map (List.map make_map_range) splited |> List.map (List.sort (fun {map = _; range = (l, _)} {map = _; range = (r, _)} -> l - r)) in
+  
+  let final_ranges = 
+    List.fold_left (fun lb_ub_s map_ranges ->
+      get_mapped_ranges map_ranges lb_ub_s |> List.sort(fun (l, _) (r, _) -> l - r)
+    ) lb_ub_seeds all_map_ranges in
+  List.fold_left (fun prev (lb, _) -> if lb < prev then lb else prev) (Int.max_int) final_ranges |>
 
-      let total = map prev in
-      print_endline ("(+) " ^ string_of_int (map 0) ^ "\t->\t" ^ string_of_int total);
-      total
-    ) seed map_ranges
-    
-  ) seeds in 
-  List.fold_left (fun prev curr -> if curr < prev then curr else prev) (List.nth locations 0) locations |>
   string_of_int |>
-  print_endline
-  (*List.iter (fun s -> print_endline ""; List.iter (print_endline) s) splited;*)
+  print_endline;
   
   
   
 ;;
+
